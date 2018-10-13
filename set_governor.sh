10abc7bfeb90ca87eec6140dc2a89c3e13aab9b0
bunnie <bunnie@alphamaxmedia.com>
2018-10-13 11:10:29 -0400

    output the current governor status before setting governor

--- set_governor.sh
+++ set_governor.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo performance > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo performance > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
