54aff2855b1cba56eb6aeb6c5cc5e477da7fd3d6
bunnie <bunnie@kosagi.com>
2018-10-30 04:31:01 -0400

    add pm2 shell scripts the default netv2 build relies upon

---
+++ mm.sh
@@ -0,0 +1,3 @@
+cd ~/code/MagicMirror
+sleep 10 # give 10 seconds to pull up a console and quit
+DISPLAY=:0 npm start
---
+++ netv2-status.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+node /home/pi/code/netv2mvp-scripts/magicmirror/netv2-status/netv2-status.js
